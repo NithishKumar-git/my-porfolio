cec638c00bb98a1f83d62c571db04592d1afb027
unknown <snithishk7@gmail.com>
2025-10-10 09:20:20 +0530

    Your message here

---
+++ js/js.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll('.html, .css, .py'); // animate all skill boxes
+
+  function showOnScroll() {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    elements.forEach(el => {
+      const rect = el.getBoundingClientRect().top;
+      if (rect < triggerBottom) {
+        el.classList.add('show');
+      } else {
+        el.classList.remove('show');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', showOnScroll);
+  showOnScroll();
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll('.edu-card'); 
+
+  function showOnScroll() {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    elements.forEach(el => {
+      const rect = el.getBoundingClientRect().top;
+      if (rect < triggerBottom) {
+        el.classList.add('show');
+      } else {
+        el.classList.remove('show');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', showOnScroll);
+  showOnScroll();
+});
